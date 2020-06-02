98a0006ac0885f77b1181a35f04b7c3069a8ab29
nuesslerm <markus@choco.com>
2020-06-03 00:37:47 +0200

    linked index.css stylesheet to index.html directly, removed it from import into index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 
 import App from './App.js';
 
-import './index.css';
+// ? import './index.css';
+// * we don't need to import ./index.css into index.js, because we already imported
+// * the css file into index.html via:
+// * <link rel="stylesheet" type="text/css" href="/src/index.css" />
 
 const wrapper = document.getElementById('root');
 wrapper ? ReactDOM.render(<App />, wrapper) : false;
