e46a50d67175a768f558e05fda1318acbff19dac
nuesslerm <markus@choco.com>
2020-06-04 01:20:07 +0200

    setup bootstrap4 for webpack4

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,18 @@
 // https://github.com/pixelgoo/simple_webpack_boilerplate,
 // https://dev.to/pixelgoo/how-to-configure-webpack-from-scratch-for-a-basic-website-46a5
 
+// boostrap 4 and font-awespme setup tutorial:
+// for webpack 4: https://medium.com/@estherfalayi/webpack-4-nicknamed-legato-is-out-and-the-community-is-still-on-the-hots-for-it-i-am-too-e9bee3d2e4a
+// // webpack 3: https://medium.com/@estherfalayi/setting-up-webpack-for-bootstrap-4-and-font-awesome-eb276e04aaeb
+
+const webpack = require('webpack');
 const path = require('path');
 
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: ['font-awesome/scss/font-awesome.scss', './src/index.js'],
 
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -59,6 +64,25 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'url-loader?limit=10000',
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          'file-loader?name=images/[name].[ext]',
+          'image-webpack-loader?bypassOnDebug',
+        ],
+      },
+      {
+        test: /bootstrap\/dist\/js\/umd\//,
+        use: 'imports-loader?jQuery=jquery',
+      },
+      {
+        test: /font-awesome\.config\.js/,
+        use: [{ loader: 'style-loader' }, { loader: 'font-awesome-loader' }],
+      },
     ],
   },
 
@@ -74,6 +98,27 @@ module.exports = {
       // filename doesn't need to be specified; will be main.css
       filename: '[name].css',
       // chunkFilename: '[id].css',
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      tether: 'tether',
+      Tether: 'tether',
+      'window.Tether': 'tether',
+      Popper: ['popper.js', 'default'],
+      'window.Tether': 'tether',
+      Alert: 'exports-loader?Alert!bootstrap/js/dist/alert',
+      Button: 'exports-loader?Button!bootstrap/js/dist/button',
+      Carousel: 'exports-loader?Carousel!bootstrap/js/dist/carousel',
+      Collapse: 'exports-loader?Collapse!bootstrap/js/dist/collapse',
+      Dropdown: 'exports-loader?Dropdown!bootstrap/js/dist/dropdown',
+      Modal: 'exports-loader?Modal!bootstrap/js/dist/modal',
+      Popover: 'exports-loader?Popover!bootstrap/js/dist/popover',
+      Scrollspy: 'exports-loader?Scrollspy!bootstrap/js/dist/scrollspy',
+      Tab: 'exports-loader?Tab!bootstrap/js/dist/tab',
+      Tooltip: 'exports-loader?Tooltip!bootstrap/js/dist/tooltip',
+      Util: 'exports-loader?Util!bootstrap/js/dist/util',
     }),
   ],
 };
