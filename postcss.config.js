c141517107f798f0fc3f1cdaedaf5928850b5913
nuesslerm <markus@choco.com>
2020-06-04 10:00:26 +0200

    setup bootstrap4 for webpack4 5

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,16 @@
 // It is handy to not have those transformations while we are developing
 if (process.env.NODE_ENV !== 'production') {
   module.exports = {
+    /**
     plugins: [
       require('autoprefixer'),
       require('cssnano'),
       // More postCSS modules here if needed
     ],
+    **/
+    plugins: function () {
+      // post css plugins, can be exported to postcss.config.js
+      return [require('cssnano'), require('precss'), require('autoprefixer')];
+    },
   };
 }
