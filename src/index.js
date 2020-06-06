d3b838ea023dd64754328d88e97969e40fff7807
nuesslerm <markus@choco.com>
2020-06-06 03:01:12 +0200

    bootstrap tut 1

--- src/index.js
+++ src/index.js
@@ -8,16 +8,17 @@ import App from './react/App.js';
 // * the css file into index.html via:
 // * <link rel="stylesheet" type="text/css" href="/src/index.css" />
 
-// ? import 'bootstrap';
+// import 'bootstrap';
 // I'm not importing the entire bootstrap, because I'm already getting the styles
 // from './scss/custom.scss'
 
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 // './scss/custom.scss' needs to come after 'bootstrap/dist/css/bootstrap.min.css'
 // because of css imports also follow cascading pattern
 import './bootstrap-styles/custom.scss';
+import './index.css';
 
 const wrapper = document.getElementById('root');
 wrapper ? ReactDOM.render(<App />, wrapper) : false;
