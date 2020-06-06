e249c1074d23b1bafccec8a607c22e438f33c8b1
nuesslerm <markus@choco.com>
2020-06-06 03:05:41 +0200

    bootstrap tut 2

--- src/index.js
+++ src/index.js
@@ -7,12 +7,15 @@ import App from './react/App.js';
 // * we don't need to import ./index.css into index.js, because we already imported
 // * the css file into index.html via:
 // * <link rel="stylesheet" type="text/css" href="/src/index.css" />
+// ! actually we do need to import './index.css' at the very end to leverage our
+// ! custom css over default bootstrap styles, as defined in
+// ! 'bootstrap/dist/css/bootstrap.min.css' and './bootstrap-styles/custom.scss'
 
 // import 'bootstrap';
 // I'm not importing the entire bootstrap, because I'm already getting the styles
 // from './scss/custom.scss'
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 // './scss/custom.scss' needs to come after 'bootstrap/dist/css/bootstrap.min.css'
