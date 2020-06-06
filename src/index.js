4c83c0f1ab118d01993e9f3aecf1798f6a2e7742
nuesslerm <markus@choco.com>
2020-06-07 01:28:05 +0200

    changed custom stylesheet from an index.css to and index.scss file

--- src/index.js
+++ src/index.js
@@ -21,7 +21,8 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 // './scss/custom.scss' needs to come after 'bootstrap/dist/css/bootstrap.min.css'
 // because of css imports also follow cascading pattern
 import './bootstrap-styles/custom.scss';
-import './index.css';
+// import your own style-sheet as './index.css' or './index.scss'
+import './index.scss';
 
 const wrapper = document.getElementById('root');
 wrapper ? ReactDOM.render(<App />, wrapper) : false;
